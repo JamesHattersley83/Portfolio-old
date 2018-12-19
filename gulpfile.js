fdb7ef73d25e3c3746065f2da2e42d8ad3c56133
jhatts <jhatts@hotmail.co.uk>
2018-12-19 21:43:50 +0000

    moved index.html file into src folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,21 @@ const sass = require('gulp-sass');
 
 // compile sass
 gulp.task('sass', function() {
-    return gulp.src(['src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.stream())
+  return gulp
+    .src(['src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
 });
 
 // watch and serve
-gulp.task('serve', ['sass'], function(){
-    browserSync.init({
-        server: './src'
-    });
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: './src'
+  });
 
-    gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch(['src/*.html']).on('change', browserSync.reload);
+  gulp.watch(['src/scss/*.scss'], ['sass']);
+  gulp.watch(['src/*.html']).on('change', browserSync.reload);
 });
 
 // Default
